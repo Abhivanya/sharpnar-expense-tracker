f1ac16ccda8eaf4c5de337a71487dae885f0af01
Abhivanya <your_email@example.com>
2024-12-08 22:52:40 +0530

    add logout functionality

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <header className="flex w-full justify-between  items-center p-7 text-black">
       <div>Welcome to Expnese Tracker!</div>
-      <div>
-        Your Profile is Incomplete{" "}
-        <NavLink to="/profile" className="underline px-1 pointer text-blue-600">
-          Complete now
-        </NavLink>
+      <div className="flex justify-evenly items-center gap-3">
+        <div>
+          Your Profile is Incomplete{" "}
+          <NavLink
+            to="/profile"
+            className="underline px-1 pointer text-blue-600"
+          >
+            Complete now
+          </NavLink>
+        </div>
+
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 text-white px-3 py-1 rounded-md hover:text-red-500 hover:bg-yellow-100"
+        >
+          Logout
+        </button>
       </div>
     </header>
   );
