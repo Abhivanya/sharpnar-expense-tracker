494b84aabfe492f382b916cf69c9982f2ba9c091
Abhivanya <your_email@example.com>
2024-12-09 14:05:58 +0530

    integrated with firebase

--- src/components/Expences.jsx
+++ src/components/Expences.jsx
@@ -1,25 +1,59 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const Expences = () => {
-  const [expences, setExpences] = useState([
-    { id: 1, amount: 300, description: "test", category: "food" },
-  ]);
+  const [expences, setExpences] = useState({});
 
+  const [refetch, setRefetch] = useState(false);
   const amountRef = useRef();
   const desRef = useRef();
   const categoryRef = useRef();
 
+  useEffect(() => {
+    fetch("https://wheatherapp-f9067-default-rtdb.firebaseio.com/expences.json")
+      .then((response) => response.json())
+      .then((res) => {
+        if (res.error) {
+          throw new Error(res.error);
+        }
+        console.log(res);
+        setExpences(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err.message);
+      });
+  }, [refetch]);
+
   const addExpense = (e) => {
     e.preventDefault();
-    setExpences((prev) => [
-      ...prev,
+    fetch(
+      "https://wheatherapp-f9067-default-rtdb.firebaseio.com/expences.json",
       {
-        id: date.now(),
-        amount: amountRef.current.value,
-        description: desRef.current.value,
-        category: categoryRef.current.value,
-      },
-    ]);
+        method: "POST",
+        body: JSON.stringify({
+          amount: amountRef.current.value,
+          description: desRef.current.value,
+          category: categoryRef.current.value,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        if (res.error) {
+          throw new Error(res.error.message);
+        }
+        console.log(res);
+        alert("Expense Added successfully");
+        setRefetch(!refetch);
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err);
+      });
+    e.target.reset();
   };
   return (
     <div className="flex flex-col  w-[90%] mx-[auto] p-5 bg-yellow-100 rounded-md">
@@ -47,19 +81,21 @@ const Expences = () => {
           Add Expense
         </button>
       </form>
-      <div>
-        <h1 className="text-3xl text-blue-500 underline underline-offset-4 text-center">
+      <div className="mt-8">
+        <h1 className="text-3xl text-blue-500 underline underline-offset-4 text-center font-bold">
           Expences
         </h1>
-        <div className="flex w-[90%] bg-green-800">
-          helo
-          {expences.map((expence) => {
-            <div key={expence.id}>
-              <div>{expence.description}</div>
-              <div>{expence.amount}</div>
-              <div>{expence.category}</div>
-            </div>;
-          })}
+        <div className="flex w-[99%] min-h-[400px] p-4 mt-4 bg-green-300 rounded-md gap-2 flex-wrap">
+          {Object.entries(expences).map(([key, expense]) => (
+            <div
+              key={key}
+              className="border-2 border-blue-400 p-3 rounded-md w-[200px] h-[150px] flex flex-col font-semibold"
+            >
+              <div>{expense.description}</div>
+              <div>{expense.amount}</div>
+              <div>{expense.category}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
